d4d7b379447b4139e70809a14a612b4d67aca7a7
DariaIliuchenko <102221418+DariaIliuchenko@users.noreply.github.com>
2022-12-26 13:21:52 +0200

    done

--- helpers/HTTPError.js
+++ helpers/HTTPError.js
@@ -5,13 +5,6 @@ const messages = {
     404: "Not found",
     409: "Conflict"
 }
-
-
-
-
-
-
-
 const HTTPError = (status, message = messages[status]) => {
     const error = new Error(message);
       error.status = status;
--- helpers/index.js
+++ helpers/index.js
@@ -1,6 +1,8 @@
 const HTTPError = require("./HTTPError");
 const ctrlWrapper = require("./ctrlWrapper")
+const handleSaveErrors = require("./handleSaveErrors");
 module.exports = {
     HTTPError,
     ctrlWrapper,
+    handleSaveErrors,
 }
